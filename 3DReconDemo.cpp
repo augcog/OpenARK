c5e5ed2f09a8e027d2cfe85da40ce2c1c60c5236
Adam Chang <adamchang2000@berkeley.edu>
2020-03-09 15:43:31 -0700

    added open3d to dependencies, added 3drecon to readme, minor print change to 3drecondemo

--- 3DReconDemo.cpp
+++ 3DReconDemo.cpp
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
 {
 
 	if (argc > 5) {
-		std::cerr << "Usage: ./" << argv[0] << " configuration-yaml-file [vocabulary-file] [skip-first-seconds]" << std::endl
+		std::cerr << "Usage: ./" << argv[0] << " [configuration-yaml-file] [vocabulary-file] [frame output directory]" << std::endl
 			<< "Args given: " << argc << std::endl;
 		return -1;
 	}
