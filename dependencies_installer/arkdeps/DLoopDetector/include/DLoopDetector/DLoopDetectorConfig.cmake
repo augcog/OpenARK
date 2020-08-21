1ffa6a662740db4eb6bc8fc9b55f333510e07428
woojinko <kwj924@gmail.com>
2020-08-21 12:34:20 -0700

    removed dependency installer creation process but added the installer executable itself to the master repo

--- dependencies_installer/arkdeps/DLoopDetector/include/DLoopDetector/DLoopDetectorConfig.cmake
+++
@@ -1,4 +0,0 @@
-get_filename_component(DLoopDetector_CMake_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-SET(DLoopDetector_LIBRARIES "")
-SET(DLoopDetector_LIBS ${DLoopDetector_LIBRARY})
-SET(DLoopDetector_INCLUDE_DIRS "${DLoopDetector_CMake_DIR}")
--- dependencies_installer/arkdeps/DLoopDetector/lib/cmake/DLoopDetector/DLoopDetectorConfig.cmake
+++
@@ -1,9 +0,0 @@
-FIND_LIBRARY(DLoopDetector_LIBRARY DLoopDetector
-    PATHS C:/Program Files/DLoopDetector/lib
-)
-FIND_PATH(DLoopDetector_INCLUDE_DIR DLoopDetectorConfig.cmake
-    PATHS C:/Program Files/DLoopDetector/include/DLoopDetector 
-)
-SET(DLoopDetector_LIBRARIES ${DLoopDetector_LIBRARY})
-SET(DLoopDetector_LIBS ${DLoopDetector_LIBRARY})
-SET(DLoopDetector_INCLUDE_DIRS ${DLoopDetector_INCLUDE_DIR})
