4e1a30b312392f13dcf5eec8ef256c06abeaf2fe
Adam Chang <adamchang2000@berkeley.edu>
2021-04-23 12:48:37 -0700

    removed some incorrect comments after changes

--- include/SegmentedMesh.h
+++ include/SegmentedMesh.h
@@ -63,7 +63,7 @@ namespace ark {
 		std::tuple<std::shared_ptr<std::vector<std::vector<Eigen::Vector3d>>>, 
 			std::shared_ptr<std::vector<std::vector<Eigen::Vector3d>>>,
 			std::shared_ptr<std::vector<std::vector<Eigen::Vector3i>>>, 
-			std::shared_ptr<std::vector<Eigen::Matrix4d>>, std::shared_ptr<std::vector<int>>> GetOutputVectors(); // Moon: applied eigen syntax in PR (all-inclusive)
+			std::shared_ptr<std::vector<Eigen::Matrix4d>>, std::shared_ptr<std::vector<int>>> GetOutputVectors();
 
 		void AddRenderMutex(std::mutex* render_mutex, std::string render_mutex_key);
 		void RemoveRenderMutex(std::string render_mutex_key);
