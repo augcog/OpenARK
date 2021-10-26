145ad3c86f1e6d4831d4ea6ed925e1042d34b09c
Tygen <tygensun@gmail.com>
2021-10-26 11:46:35 -0700

    HumanDetector cleaned

--- include/HumanBody.h
+++ include/HumanBody.h
@@ -2,18 +2,18 @@
 #include "FrameObject.h"
 
 namespace ark {
-	/** Class representing a human body in the current frame
-	* @see HumanBody.cpp
-	*/
-	class HumanBody
-	{
-	public:
-		HumanBody();
-		
-		~HumanBody();
+    /** Class representing a human body in the current frame
+    * @see HumanBody.cpp
+    */
+    class HumanBody
+    {
+    public:
+        HumanBody();
+        
+        ~HumanBody();
 
-		std::vector<cv::Point> MPIISkeleton2D;
+        std::vector<cv::Point> MPIISkeleton2D;
 
-		cv::Vec3f headDirection;
-	};
+        cv::Vec3f headDirection;
+    };
 }
