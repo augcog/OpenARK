069aca11958a423e2263e3c3122c3b26df25aa97
Tygen <tygensun@gmail.com>
2021-11-28 17:24:00 -0800

    Update PoseGraphSolver.cpp

--- src/PoseGraphSolver.cpp
+++ src/PoseGraphSolver.cpp
@@ -60,39 +60,6 @@ namespace ark {
             new PoseError(C_AB));
     }
 
-    template <typename T>
-    bool ark::PoseError::operator()(const T* const p_a_ptr, const T* const q_a_ptr, const T* const p_b_ptr, const T* const q_b_ptr, T* residuals_ptr) const
-    {
-        Eigen::Map<const Eigen::Matrix<T, 3, 1> > p_a(p_a_ptr);
-        Eigen::Map<const Eigen::Quaternion<T> > q_a(q_a_ptr);
-
-        Eigen::Map<const Eigen::Matrix<T, 3, 1> > p_b(p_b_ptr);
-        Eigen::Map<const Eigen::Quaternion<T> > q_b(q_b_ptr);
-
-        // Compute the relative transformation between the two frames.
-        Eigen::Quaternion<T> q_a_inverse = q_a.conjugate();
-        Eigen::Quaternion<T> q_ab_estimated = q_a_inverse * q_b;
-
-        // Represent the displacement between the two frames in the A frame.
-        Eigen::Matrix<T, 3, 1> p_ab_estimated = q_a_inverse * (p_b - p_a);
-
-        // Compute the error between the two orientation estimates.
-        Eigen::Quaternion<T> delta_q =
-            q_ab_measured_.template cast<T>() * q_ab_estimated.conjugate();
-
-        // Compute the residuals.
-        // [ position         ]   [ delta_p          ]
-        // [ orientation (3x1)] = [ 2 * delta_q(0:2) ]
-        Eigen::Map<Eigen::Matrix<T, 6, 1> > residuals(residuals_ptr);
-        residuals.template block<3, 1>(0, 0) =
-            p_ab_estimated - p_ab_measured_.template cast<T>();
-        residuals.template block<3, 1>(3, 0) = T(2.0) * delta_q.vec();
-
-        // Scale the residuals by the measurement uncertainty.
-        residuals.applyOnTheLeft(sqrt_information_.template cast<T>());
-
-        return true;
-    }
 
     SimplePoseGraphSolver::SimplePoseGraphSolver() :
         optimizing(false), loopQueued(false)
